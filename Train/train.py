48620f3aaf5a8c11d42721d20c2fa9af6a91371c
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-08-31 23:44:26 -0300

    Exportando model info

--- Train/train.py
+++ Train/train.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 def importDataSet():
   ssl._create_default_https_context = ssl._create_unverified_context
-  url = 'https://raw.githubusercontent.com/flavio185/MLEngineer/main/salary_case/ds_salaries.csv'
+  url = 'https://raw.githubusercontent.com/flavio185/ml-salary-predict/main/Train/ds_salaries.csv'
   dataset = pd.read_csv(url)
   return dataset
 
@@ -28,13 +28,13 @@ df["work_year"] = df["work_year"].astype("str")
 # Renaming columns entries for clarity
 
 # Experience Level
-df["experience_level"] = df["experience_level"].map({"SE":"Senior", "MI":"Junior", "EN":"Entry", "EX":"Expert"})
+#df["experience_level"] = df["experience_level"].map({"SE":"Senior", "MI":"Junior", "EN":"Entry", "EX":"Expert"})
 # # Employment Type
-df["employment_type"] = df["employment_type"].map({"FT":"Full-time", "PT":"Part-time", "CT":"Contract", "FL":"Freelance"})
+#df["employment_type"] = df["employment_type"].map({"FT":"Full-time", "PT":"Part-time", "CT":"Contract", "FL":"Freelance"})
 # # Remote Ratio
 df["remote_ratio"] = df["remote_ratio"].map({0:"Onsite", 50:"Hybrid", 100:"Remote"})
 # Company Size
-df["company_size"] = df["company_size"].map({"S":"Small", "M":"Medium", "L":"Large"})
+#df["company_size"] = df["company_size"].map({"S":"Small", "M":"Medium", "L":"Large"})
 
 ###PRE PROCESSING
 
@@ -49,9 +49,9 @@ X.drop(["salary_currency", "company_location"], axis = 1, inplace = True)
 X_nominal_data = X[["work_year", "job_title", "remote_ratio", "employee_residence"]].reset_index(drop=True)
 
 X_ordinal_data = X[["experience_level", "employment_type", "company_size"]].reset_index(drop=True)
-X_ordinal_data["experience_level"] = X_ordinal_data["experience_level"].map({"Entry":1, "Junior":2, "Senior":3, "Expert":4})
-X_ordinal_data["employment_type"] = X_ordinal_data["employment_type"].map({"Freelance":1, "Contract":2, "Part-time":3, "Full-time":4})
-X_ordinal_data["company_size"] = X_ordinal_data["company_size"].map({"Small":1, "Medium":2, "Large":3})
+X_ordinal_data["experience_level"] = X_ordinal_data["experience_level"].map({"EN":1, "MI":2, "SE":3, "EX":4})
+X_ordinal_data["employment_type"] = X_ordinal_data["employment_type"].map({"FL":1, "CT":2, "PT":3, "FT":4})
+X_ordinal_data["company_size"] = X_ordinal_data["company_size"].map({"S":1, "M":2, "L":3})
 
 enc = OneHotEncoder(drop="first")
 
@@ -72,7 +72,35 @@ svr = SVR()
 
 model = SVR(C = 1, gamma = 1, kernel = "poly")
 model.fit(X_train, y_train)
-#preds = model.predict(X_valid[:1])
-
+preds = model.predict(X_valid[:1])
+print(preds)
 dump(model, 'model.joblib') # save the model
 dump(enc, 'encoder.joblib') # save the encoder
+
+
+
+
+import json
+from sklearn.model_selection import cross_val_score
+
+import sklearn
+import sys
+
+def validatingModel():
+  scores = -1 * cross_val_score(model, X, y, scoring = "neg_mean_absolute_error", cv = 10)
+  return scores.mean()
+
+def generateMetrics():
+  metadata = {
+      "python": sys.version,
+      "Versao sklearn": sklearn.__version__,      
+      "Model Name": type(model).__name__,
+      "Model info": model.get_params(),
+      "MEA": validatingModel(),
+  }
+  return json.dumps( metadata )
+
+metrics = generateMetrics()
+# Writing to sample.json
+with open("metadata.json", "w") as outfile:
+    outfile.write(metrics)
