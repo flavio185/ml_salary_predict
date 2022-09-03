212fc88bbbd0d623fb20c4c348fb240836cd9258
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-09-03 13:18:09 -0300

    Atualizacao azure experiment

--- Train/azure_experiment.py
+++ Train/azure_experiment.py
@@ -1,27 +1,68 @@
 import os
+from xml.etree.ElementTree import QName
 from azureml.core import Workspace
 from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core import Experiment
 
-#Trocar os códigos abaixo pelos da sua instância!
-subscription_id = os.getenv('AZURE_SUBSCRIPTION_ID')
-resource_group = os.getenv('RESOURCE_GROUP')
-workspace_name = os.getenv('WORKSPACE_NAME')
+#
+import json
 
-svc_pr_password = os.environ.get("AZURE_CLIENT_SECRET")
-svc_pr_id = os.environ.get("AZURE_CLIENT_ID")
-tn_id = os.environ.get("AZURE_TENANT_ID")
+def connectToAzureMLWorkspace():
+    #Trocar os códigos abaixo pelos da sua instância!
+    subscription_id = os.getenv('AZURE_SUBSCRIPTION_ID')
+    resource_group = os.getenv('RESOURCE_GROUP')
+    workspace_name = os.getenv('WORKSPACE_NAME')
 
-svc_pr = ServicePrincipalAuthentication(
-    tenant_id=tn_id,
-    service_principal_id=svc_pr_id,
-    service_principal_password=svc_pr_password)
+    svc_pr_password = os.environ.get("AZURE_CLIENT_SECRET")
+    svc_pr_id = os.environ.get("AZURE_CLIENT_ID")
+    tnt_id = os.environ.get("AZURE_TENANT_ID")
+
+    svc_pr = ServicePrincipalAuthentication(
+        tenant_id=tnt_id,
+        service_principal_id=svc_pr_id,
+        service_principal_password=svc_pr_password)
 
 
-ws = Workspace(
-    subscription_id,
-    resource_group,
-    workspace_name,
-    auth=svc_pr
-    )
+    ws = Workspace(
+        subscription_id,
+        resource_group,
+        workspace_name,
+        auth=svc_pr
+        )
 
-print("Found workspace {} at location {}".format(ws.name, ws.location))
+    print("Found workspace {} at location {}".format(ws.name, ws.location))
+    return ws
+
+def startExperiment(ws, experiment_name):
+    experiment = Experiment(workspace=ws, name=experiment_name)
+    run = experiment.start_logging(run_id=str(uuid.uuid1()),
+                            display_name="My Run " + str(uuid.uuid1()),
+                            outputs="output",
+                            snapshot_directory="output")
+    return run
+    
+def logToExperiment(experiment, key, value):
+    experiment.log(key, value)
+
+def readMetadataJson(filename):
+    f = open(filename)
+    data = json.load(f)
+    return data
+    
+#Main
+metadata_json = readMetadataJson("output/metadata.json")
+#
+ws = connectToAzureMLWorkspace()
+my_experiment = startExperiment(ws, "data_science_salary_predict")
+#log json into experiment.
+for key in metadata_json:   
+    value = metadata_json[key]
+    logToExperiment(my_experiment, key, value)
+
+#Complete job.
+my_experiment.complete()
+
+    
+#json load
+
+
