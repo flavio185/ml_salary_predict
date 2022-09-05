9bbdff8cd1ae968b86cf646c1efb38be6cc2ecf1
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-09-05 07:31:39 -0300

    Alteraçao de azure_experiment para adiiconar target dir para download do modelo

--- API/downloadModel.py
+++ API/downloadModel.py
@@ -3,4 +3,4 @@ from azure_experiment import AzureModel
 #Main
 model_name = "data_science_salary_predict"
 m1 = AzureModel()
-m1.downloadModelArtifacts(model_name)
+m1.downloadModelArtifacts(model_name, target_dir="app", version=3)
