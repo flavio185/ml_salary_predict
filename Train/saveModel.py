40aabb4c6aeaf3d7eb40e3900edc608b0be857a0
flavio185 <flavio185@gmail.com>
2022-09-08 23:17:45 -0300

    Update saveModel.py

--- Train/saveModel.py
+++ Train/saveModel.py
@@ -1,9 +1,13 @@
 from azure_experiment import AzureModel 
 
 #Main
-model_name = "data_science_salary_predict"
-m1 = AzureModel()
-m1.startExperimentRun(model_name)
-m1.logMetadataJson()
-m1.finishExperimentRun()
-m1.registerModelArtifacts()
+def saveModelArtifacts(model_name):
+  model_name = "data_science_salary_predict"
+  m1 = AzureModel()
+  m1.startExperimentRun(model_name)
+  m1.logMetadataJson()
+  m1.finishExperimentRun()
+  m1.registerModelArtifacts()
+
+if __name__ == "__main__":
+    saveModelArtifacts(sys.argv)
