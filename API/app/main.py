c99f6d99f0ff6d6ed4fd41ffdd722ebcf74cdbdc
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-12-18 08:49:04 -0300

    Corrigindo versao.

--- API/app/main.py
+++ API/app/main.py
@@ -8,8 +8,9 @@ from . import predict
 app = FastAPI()
 
 model_version = {}
-with open("model_version.py") as fp:
-    exec(fp.read(), model_version)
+with open("/code/app/model_version.py") as file: 
+  model_version=file.read()
+  model_version=model_version.split('=')[1].strip()
 
 @app.get('/api')
 async def index():
@@ -18,18 +19,18 @@ async def index():
 
 @app.post("/api")
 async def api(info : Request):
-    data = await info.json()
-    experience_level = str(data['experience_level'])
-    employment_type = str(data['employment_type'])
-    company_size = str(data['company_size'])
-    work_year= str(data['work_year'])
-    job_title = str(data['job_title'])
-    remote_ratio = str(data['remote_ratio'])
-    employee_residence = str(data['employee_residence'])
+  data = await info.json()
+  experience_level = str(data['experience_level'])
+  employment_type = str(data['employment_type'])
+  company_size = str(data['company_size'])
+  work_year= str(data['work_year'])
+  job_title = str(data['job_title'])
+  remote_ratio = str(data['remote_ratio'])
+  employee_residence = str(data['employee_residence'])
 
-    output = predict.predict(experience_level, employment_type, company_size, work_year, job_title, remote_ratio, employee_residence)
-    print(output)
-    return output[0]
+  output = predict.predict(experience_level, employment_type, company_size, work_year, job_title, remote_ratio, employee_residence)
+  print(output)
+  return output[0]
 
 if __name__ == '__main__':
   uvicorn.run(app,host="127.0.0.1",port=8000)
