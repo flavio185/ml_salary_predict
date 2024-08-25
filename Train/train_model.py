923f28a4f38003ed3261a4a4cf05e7a025679a6a
flavio185 <flavio185@gmail.com>
2024-08-25 20:23:50 -0300

    Fix.

--- Train/train_model.py
+++ Train/train_model.py
@@ -73,6 +73,7 @@ X_onehotencoded = pd.DataFrame(X_onehotencoded)
 
 
 X = pd.concat([X_onehotencoded, X_ordinal_data], axis=1)
+X.columns = X.columns.astype(str)
 
 ###Training
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size = 0.8, test_size = 0.2, random_state = 1)
